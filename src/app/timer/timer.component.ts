52a1a1dae61d86f35efc3980529e7ea6a6d42620
Nellia Salimova <salimova.nellia@gmail.com>
2020-07-22 14:04:08 +0300

    timer functionality

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,46 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import Timer = NodeJS.Timer;
+import {Observable, interval, timer, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit, OnDestroy {
 
+  public running: boolean = false;
+  timer$: Observable<number>;
+  timerSubscription: Subscription;
   counter: number = 0;
-  timerRef;
-  running: boolean = false;
-  startText = 'Start';
 
-  startTimer() {
-    this.running = !this.running;
-    if (this.running) {
-      this.startText = 'Stop';
-      this.timerRef = setInterval(() => {
-        this.counter += 1;
-      }, 1000);
-    } else {
-      this.startText = 'Start';
-      clearInterval(this.timerRef);
-      this.clearTimer();
+  hours: number;
+  minutes: number;
+  seconds: number;
+
+  ngOnInit(): void {
+    this.millisecondsToTime(this.counter);
+    this.timer$ = timer(1000,1000);
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  startTimer(): void {
+    this.running = true;
+    this.timerSubscription = this.timer$.subscribe(val => {
+      this.counter += 1;
+      this.millisecondsToTime(this.counter);
+    });
+  }
+
+  pauseTimer(): void {
+    this.running = false;
+    if(this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+    this.millisecondsToTime(this.counter);
+  }
+
+  stopTimer(): void {
+    this.running = false;
+    this.timerSubscription.unsubscribe();
+    this.counter = 0;
+    this.millisecondsToTime(this.counter);
+  }
+
+  resetTimer(): void {
+    if(this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+      this.counter = 0;
+      this.millisecondsToTime(this.counter);
+      this.startTimer();
     }
   }
 
-  pauseTimer() {
-    clearInterval(this.timerRef);
-    this.running = !this.running;
-    this.startText = 'Start';
+  millisecondsToTime(milliseconds: number): void {
+    this.hours = Math.floor(milliseconds / 3600);
+    this.minutes = Math.floor((milliseconds - (this.hours * 3600)) / 60);
+    this.seconds = milliseconds - (this.hours * 3600) - (this.minutes * 60);
   }
-
-  clearTimer() {
-    this.running = false;
-    this.startText = 'Start';
-    this.counter = 0;
-    clearInterval(this.timerRef);
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.timerRef);
-  }
-
 }
