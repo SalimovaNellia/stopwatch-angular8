345c31a86c11bafbd6dd7a60f1288ecd1e85bdc5
Nellia Salimova <salimova.nellia@gmail.com>
2020-07-22 11:09:11 +0300

    basic logic for buttons

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -5,11 +5,42 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent {
 
-  constructor() { }
+  counter: number = 0;
+  timerRef;
+  running: boolean = false;
+  startText = 'Start';
 
-  ngOnInit() {
+  startTimer() {
+    this.running = !this.running;
+    if (this.running) {
+      this.startText = 'Stop';
+      this.timerRef = setInterval(() => {
+        this.counter += 1;
+      }, 1000);
+    } else {
+      this.startText = 'Start';
+      clearInterval(this.timerRef);
+      this.clearTimer();
+    }
+  }
+
+  pauseTimer() {
+    clearInterval(this.timerRef);
+    this.running = !this.running;
+    this.startText = 'Start';
+  }
+
+  clearTimer() {
+    this.running = false;
+    this.startText = 'Start';
+    this.counter = 0;
+    clearInterval(this.timerRef);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerRef);
   }
 
 }
